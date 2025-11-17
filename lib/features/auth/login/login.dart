f55fa5fe12ab912c9b0d84eeb0489b26266b2bd8
mohamed hany <mobamedhany3@gmail.com>
2025-11-17 20:58:27 +0200

    Finish homeTab ui

--- lib/features/auth/login/login.dart
+++ lib/features/auth/login/login.dart
@@ -1,4 +1,3 @@
-import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
