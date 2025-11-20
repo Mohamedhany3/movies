e0e35f452cd287d9653f7601495dae301b956cfe
mohamed hany <mobamedhany3@gmail.com>
2025-11-21 01:29:41 +0200

    edit home tab

--- lib/features/main_layout/tabs/home_tab/home_tab.dart
+++ lib/features/main_layout/tabs/home_tab/home_tab.dart
@@ -1,10 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movie/core/recources/assets_manager/assets_manager.dart';
 import 'package:movie/core/recources/colors_manager/colors_manager.dart';
+import 'package:movie/core/widgets/custom_text_button.dart';
 import 'package:movie/model/moive_model.dart';
-import 'package:movie/model/movie_item.dart';
+import 'package:movie/core/widgets/movie_item.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -14,6 +16,7 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
+  int selectedIndex = 0;
   late PageController _pageController;
   int currentPage = 0;
 
@@ -57,114 +60,40 @@ class _HomeTabState extends State<HomeTab> {
                     // AvailableNow
                     Center(child: Image.asset(ImageAssets.availableNow)),
                     SizedBox(height: 21.h),
-
-                    // Movies Card
-                    SizedBox(
-                      height: 351.h,
-                      child: ListView.builder(
-                        physics: BouncingScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: MovieModel.detalisMovie.length,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) => MovieItem(
-                          widthImage: 234.w,
-                          image: MovieModel.detalisMovie[index].image,
-                          rate: MovieModel.detalisMovie[index].rate,
-                        ),
+                    // Movies Card CarouselSlider
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        disableCenter: true,
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                        viewportFraction: .55,
+                        height: 320.h,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            selectedIndex = index;
+                          });
+                        },
                       ),
+                      items: MovieModel.detalisMovie.map((movie) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return MovieItem(
+                              image: movie.image,
+                              rate: movie.rate,
+                              widthImage: 243.w,
+                            );
+                          },
+                        );
+                      }).toList(),
                     ),
-                    // Center(
-                    //   child: SizedBox(
-                    //     height: 351.h,
-                    //     child: PageView.builder(
-                    //       controller: _pageController,
-                    //       itemCount: MovieModel.detalisMovie.length,
-                    //       onPageChanged: (index) {
-                    //         setState(() {
-                    //           currentPage = index;
-                    //         });
-                    //       },
-                    //       itemBuilder: (context, index) {
-                    //         double scale = index == currentPage ? 1.0 : 0.8;
-                    //         return TweenAnimationBuilder(
-                    //           tween: Tween<double>(begin: scale, end: scale),
-                    //           duration: Duration(milliseconds: 300),
-                    //           builder: (context, value, child) {
-                    //             return Transform.scale(
-                    //               scale: value,
-                    //               child: Stack(
-                    //                 children: [
-                    //                   // Movie Card
-                    //                   Container(
-                    //                     margin: EdgeInsets.symmetric(
-                    //                       horizontal: 10,
-                    //                     ),
-                    //                     child: ClipRRect(
-                    //                       borderRadius: BorderRadius.circular(
-                    //                         20,
-                    //                       ),
-                    //                       child: Image.asset(
-                    //                         MovieModel
-                    //                             .detalisMovie[index]
-                    //                             .image,
-                    //                         fit: BoxFit.fill,
-                    //                       ),
-                    //                     ),
-                    //                   ),
-                    //                   // Rate
-                    //                   Container(
-                    //                     margin: EdgeInsets.only(
-                    //                       left: 30,
-                    //                       top: 9,
-                    //                     ),
-                    //                     height: 28,
-                    //                     width: 58,
-                    //                     decoration: BoxDecoration(
-                    //                       color: ColorsManager.black
-                    //                           .withOpacity(0.71),
-                    //                       borderRadius: BorderRadius.circular(
-                    //                         10,
-                    //                       ),
-                    //                     ),
-                    //                     child: Row(
-                    //                       mainAxisAlignment:
-                    //                           MainAxisAlignment.center,
-                    //                       children: [
-                    //                         Text(
-                    //                           MovieModel
-                    //                               .detalisMovie[index]
-                    //                               .rate,
-                    //                           style: GoogleFonts.roboto(
-                    //                             fontSize: 16.sp,
-                    //                             fontWeight: FontWeight.w400,
-                    //                             color: ColorsManager.white,
-                    //                           ),
-                    //                         ),
-                    //                         SizedBox(width: 4.w),
-                    //                         Image.asset(
-                    //                           ImageAssets.star,
-                    //                           width: 15.w,
-                    //                           height: 15.h,
-                    //                         ),
-                    //                       ],
-                    //                     ),
-                    //                   ),
-                    //                 ],
-                    //               ),
-                    //             );
-                    //           },
-                    //         );
-                    //       },
-                    //     ),
-                    //   ),
-                    // ),
+
                     SizedBox(height: 21.h),
                     // WatchNow
                     Image.asset(ImageAssets.watchNow),
                     SizedBox(height: 16.h),
                     // TODO button SeeMore
                     Padding(
-                      padding: REdgeInsets.only(left: 16, right: 19),
+                      padding: REdgeInsets.only(left: 16, right: 12),
                       child: Row(
                         children: [
                           Text(
@@ -176,12 +105,22 @@ class _HomeTabState extends State<HomeTab> {
                             ),
                           ),
                           Spacer(),
-                          Text(
-                            'Action',
-                            style: GoogleFonts.roboto(
-                              fontSize: 20.sp,
-                              fontWeight: FontWeight.w400,
-                              color: ColorsManager.white,
+                          //See More
+                          GestureDetector(
+                            onTap: () {},
+                            child: Row(
+                              children: [
+                                CustomTextButton(
+                                  title: "See More",
+                                  onTap: () {},
+                                ),
+                                SizedBox(width: 3.w),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  size: 17,
+                                  color: ColorsManager.yellow,
+                                ),
+                              ],
                             ),
                           ),
                         ],
